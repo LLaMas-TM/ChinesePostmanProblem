d7e4740deddc6e167c8882b5eb7d00f88fcaf23a
Karol Stawowski <stawowski.dev@gmail.com>
2025-01-13 17:53:59 +0100

    fix: fix feature flag

--- feature_flags.py
+++ feature_flags.py
@@ -1,10 +1,12 @@
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 FEATURE_FLAGS = {
     "RED_LINE": os.getenv("RED_LINE") == "true",
 }
+
 
 def is_feature_enabled(flag_name: str) -> bool:
     return FEATURE_FLAGS.get(flag_name, False)
